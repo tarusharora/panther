071e834d0d54a5062b026951e8d5bda472733f77
Tarush Arora <tarusharora@gmail.com>
2019-04-07 17:25:14 +0530

    Add basic routing with redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import Home from './views/home'
+import About from './views/home/about'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/about-us">About</Link>
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
+  </div>
+)
 
 export default App;
